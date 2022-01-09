f38b16a1b75d2e58752e036c7098f1847f39b763
yuanx <1220998011@qq.com>
2022-01-09 16:10:13 +0800

    fix: 注释

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ module.exports = {
   aid: '',
   uuid: '',
   _signature: '',
-  DING_TALK_TOKEN: '',
+  DING_TALK_TOKEN: '', // 钉钉webhook参考（很简单）：https://open.dingtalk.com/document/group/custom-robot-access
   uid: '', // *自动玩游戏需要此参数，在掘金首页打开控制台输入这行代码`window.__NUXT__.state.auth.user.id`就可以得到
 }
